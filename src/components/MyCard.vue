75a78ca99b8e05c9d2e5b640a5bd8cffb7fab259
Tanya Sattaya-aphitan <tanlull@gmail.com>
2020-07-27 22:14:01 +0700

    revert to final

--- src/components/MyCard.vue
+++ src/components/MyCard.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="card mb-3" style="min-width:400px;max-width:538px;">
-    <div class="row no-gutters">
-      <div class="col-md-4">
-        <img :src="bookDetail.cover" class="card-img" alt />
+  <div class="card">
+    <img :src="bookDetail.cover" class="card-img-top" alt />
+    <div class="card-body">
+      <h5 class="card-title">{{bookDetail.name}}</h5>
+      <p class="card-text">{{bookDetail.description}}</p>
+      <p class="card-text">
+        <small class="text-muted">{{bookDetail.author}}</small>
+      </p>
+      <div class="small-box">
+        <small class="text-small">{{bookDetail.date}}</small>
+        <small class="text-small rating">{{bookDetail.rating}}</small>
       </div>
+<<<<<<< HEAD
       <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">{{bookDetail.name}}</h5>
@@ -20,6 +28,11 @@
             <a href="#" class="btn btn-danger">DELETE</a>
           </div>
         </div>
+=======
+      <div class="small-box">
+        <router-link :to="'/edit-book/'+index" class="btn btn-primary">EDIT</router-link>
+        <a href="#" @click="deleteBook(index)" class="btn btn-danger">DELETE</a>
+>>>>>>> parent of fb34a74... done
       </div>
     </div>
   </div>
@@ -28,7 +41,7 @@
 <script>
 export default {
   name: "MyCard",
-  props: ["bookDetail", "index"],
+  props: ["bookDetail","index"],
   methods: {
 
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
+<<<<<<< HEAD
     <h1>MY HOME PAGE</h1>
+=======
+    <h1>My Home Page</h1>
+    <div class="container">
+      <div class="card-columns">
+        <my-card 
+          v-for="(book,index) in $store.state.books"
+          :bookDetail="book"
+          :index="index"
+        />
+      </div>
+    </div>
+>>>>>>> parent of fb34a74... done
   </div>
 </template>
 
